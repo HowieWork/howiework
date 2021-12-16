2b9ca5ca185051056d8679bdd93f0f61e9abfb1f
Howie <howiewang2020@gmail.com>
2021-12-16 14:35:16 -0500

    Update section about: add section interests as a part of section about

--- src/home/components/About.js
+++ src/home/components/About.js
@@ -1,57 +1,75 @@
+import { Fragment } from 'react/cjs/react.production.min';
 import { MdKeyboardArrowRight } from 'react-icons/md';
 import Button from '../../shared/components/FormElements/Button';
 import './About.css';
 
 const About = () => {
   return (
-    <section className='responsive-width section-about'>
-      <div className='center-flex-row tiny-gap about-title'>
-        <div></div> About
-      </div>
+    <Fragment>
+      {/* ABOUT ME */}
+      <section className='responsive-width section-about'>
+        <div className='center-flex-row tiny-gap about-title'>
+          <div></div> About
+        </div>
 
-      <div className='center-flex-column tiny-gap about-container'>
-        <div className='center-flex-row medium-gap about-text'>
-          <span className='about-text--year'>2015</span>
-          <span>
-            Completed the Bachelor’s Program in China University of Mining and
-            Technology (中国矿业大学)
-          </span>
-        </div>
-        <div className='center-flex-row medium-gap about-text'>
-          <span className='about-text--year'>2015</span>
-          <span>Moved to United States from China</span>
-        </div>
-        <div className='center-flex-row medium-gap about-text'>
-          <span className='about-text--year'>2018</span>
-          <span>Completed the Master’s Program in University of Virginia.</span>
-        </div>
-        <div className='center-flex-row medium-gap about-text'>
-          <span className='about-text--year'>2018</span>
-          <span>Worked at WeWork, New York HQ</span>
-        </div>
-        <div className='center-flex-row medium-gap about-text'>
-          <span className='about-text--year'>2019</span>
-          <span>Worked at SOM, DC office</span>
-        </div>
-        <div className='center-flex-row medium-gap about-text'>
-          <span className='about-text--year'>2020 to present</span>
-          <span>Self-teaching web development</span>
-        </div>
-      </div>
-
-      {/* ABOUT CTA */}
-      <div className='about-cta'>
-        {/* FIXME UPDATE TO */}
-        <Button to='/resume.pdf' primary>
-          <div className='center-flex-row tiny-gap'>
-            Resume
-            <span className='center-flex-row'>
-              <MdKeyboardArrowRight />
+        <div className='center-flex-column tiny-gap about-container'>
+          <div className='center-flex-row medium-gap about-text'>
+            <span className='about-text--year'>2015</span>
+            <span>
+              Completed the Bachelor’s Program in China University of Mining and
+              Technology (中国矿业大学)
             </span>
           </div>
-        </Button>
-      </div>
-    </section>
+          <div className='center-flex-row medium-gap about-text'>
+            <span className='about-text--year'>2015</span>
+            <span>Moved to United States from China</span>
+          </div>
+          <div className='center-flex-row medium-gap about-text'>
+            <span className='about-text--year'>2018</span>
+            <span>
+              Completed the Master’s Program in University of Virginia.
+            </span>
+          </div>
+          <div className='center-flex-row medium-gap about-text'>
+            <span className='about-text--year'>2018</span>
+            <span>Worked at WeWork, New York HQ</span>
+          </div>
+          <div className='center-flex-row medium-gap about-text'>
+            <span className='about-text--year'>2019</span>
+            <span>Worked at SOM, DC office</span>
+          </div>
+          <div className='center-flex-row medium-gap about-text'>
+            <span className='about-text--year'>2020 to present</span>
+            <span>Self-teaching web development</span>
+          </div>
+        </div>
+
+        {/* ABOUT CTA */}
+        <div className='about-cta'>
+          {/* FIXME UPDATE TO */}
+          <Button to='/resume.pdf' primary>
+            <div className='center-flex-row tiny-gap'>
+              Resume
+              <span className='center-flex-row'>
+                <MdKeyboardArrowRight />
+              </span>
+            </div>
+          </Button>
+        </div>
+      </section>
+
+      {/* INTERESTS */}
+      <section className='responsive-width section-interests'>
+        <div className='center-flex-row tiny-gap interests-title'>
+          <div></div> Interests
+        </div>
+        <div className='interests-text'>
+          {/* TODO CHANGE SPAN TO LINK */}
+          <span>Photography</span>, Art, <span>Drawing</span>, Meditation,
+          Hiking, Gaming, Travelling
+        </div>
+      </section>
+    </Fragment>
   );
 };
 
