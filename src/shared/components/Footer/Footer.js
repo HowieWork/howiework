a6bf58fc9651252ae7ba1bd3c94f33d2aed3ca24
Howie <howiewang2020@gmail.com>
2024-08-12 14:41:36 -0400

    update current status

--- src/shared/components/Footer/Footer.js
+++ src/shared/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="center-text footer">
-      © Designed & Built by Howie Wang, 2023
+      © Designed & Built by Howie Wang, 2024
     </footer>
   );
 };
