e4b2582c1b58d07efc671bdb78c947de7016f089
Howie <howiewang2020@gmail.com>
2025-03-05 09:33:53 -0500

    update home path

--- src/App.js
+++ src/App.js
@@ -1,80 +1,80 @@
-import { useState } from 'react';
+import { useState } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
   Switch,
-} from 'react-router-dom';
+} from "react-router-dom";
 
-import MainNavigation from './shared/components/Navigation/MainNavigation';
-import Footer from './shared/components/Footer/Footer';
-import { ThemeContext } from './shared/context/theme-context';
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import Footer from "./shared/components/Footer/Footer";
+import { ThemeContext } from "./shared/context/theme-context";
 
-import Home from './home/pages/Home';
-import Projects from './portfolio/pages/Projects';
-import ProjectDetail from './portfolio/pages/ProjectDetail';
-import Posts from './post/pages/Posts';
-import PostDetail from './post/pages/PostDetail';
-import Photos from './photography/pages/Photos';
-import Drawings from './drawing/pages/Drawings';
+import Home from "./home/pages/Home";
+import Projects from "./portfolio/pages/Projects";
+import ProjectDetail from "./portfolio/pages/ProjectDetail";
+import Posts from "./post/pages/Posts";
+import PostDetail from "./post/pages/PostDetail";
+import Photos from "./photography/pages/Photos";
+import Drawings from "./drawing/pages/Drawings";
 
 const App = () => {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
 
   const toggleDarkTheme = () => {
-    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle("dark-theme");
     setIsDarkTheme((prevState) => !prevState);
   };
 
   // SYNC WITH SYSTEM COLOR THEME
-  const isSystemDark = window.matchMedia('(prefers-color-scheme: dark)');
+  const isSystemDark = window.matchMedia("(prefers-color-scheme: dark)");
 
-  isSystemDark.addEventListener('change', function (event) {
+  isSystemDark.addEventListener("change", function (event) {
     // NOTE .MATCHES RETURNS TRUE/FALSE
     event.matches ? setIsDarkTheme(true) : setIsDarkTheme(false);
   });
 
   // TOGGLE DARK THEME FOR BODY ELEMENT
   isDarkTheme
-    ? document.body.classList.add('dark-theme')
-    : document.body.classList.remove('dark-theme');
+    ? document.body.classList.add("dark-theme")
+    : document.body.classList.remove("dark-theme");
 
   const routes = (
     <Switch>
       {/* HOMEPAGE */}
-      <Route path='/' exact>
+      <Route path="/howiework" exact>
         <Home />
       </Route>
 
       {/* PORFOLIO */}
-      <Route path='/portfolio' exact>
+      <Route path="/portfolio" exact>
         <Projects />
       </Route>
-      <Route path='/portfolio/app' exact></Route>
-      <Route path='/portfolio/app/:projectSlug' exact>
+      <Route path="/portfolio/app" exact></Route>
+      <Route path="/portfolio/app/:projectSlug" exact>
         <ProjectDetail />
       </Route>
 
       {/* POSTS */}
-      <Route path='/posts' exact>
+      <Route path="/posts" exact>
         <Posts />
       </Route>
-      <Route path='/posts/:postSlug' exact>
+      <Route path="/posts/:postSlug" exact>
         <PostDetail showTitle={true} />
       </Route>
 
       {/* PHOTOGRAPHY */}
-      <Route path='/photography' exact>
+      <Route path="/photography" exact>
         <Photos />
       </Route>
 
       {/* DRAWINGS */}
-      <Route path='/drawings' exact>
+      <Route path="/drawings" exact>
         <Drawings />
       </Route>
 
       {/* REDIRECT */}
-      <Redirect to='/' />
+      <Redirect to="/" />
     </Switch>
   );
 
@@ -85,7 +85,7 @@ const App = () => {
         toggleDarkThemeHandler: toggleDarkTheme,
       }}
     >
-      <Router basename=''>
+      <Router basename="">
         <MainNavigation />
         <main>{routes}</main>
         <Footer />
